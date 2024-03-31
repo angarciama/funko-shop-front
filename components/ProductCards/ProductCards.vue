c4682a8af2401fe726ff0f9038a888a38f7e2710
angarciama <angarciama@unal.edu.co>
2024-03-30 23:34:28 -0500

    [Admin] -component productCards: avance

--- components/ProductCards/ProductCards.vue
+++ components/ProductCards/ProductCards.vue
@@ -1,24 +1,16 @@
 <template>
-  <div
-      v-for="products in products"
-      :key="products.id"
-      class="general-user-card-container">
-    {{products}}
+  <div>
+    {{ products }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import ProductsModel from '~/api/models/products/productsModel'
-import { allProducts } from '~/api/controllers/products/products.controller'
 
-const products = ref<ProductsModel[]>([])
-onMounted(async () => {
-  try {
-    products.value = await allProducts()
-    console.log(products.value)
-  } catch (error) {
-    console.error('Error fetching products:', error)
+const props = defineProps({
+  products: {
+    type: Array as () => ProductsModel[],
+    required: true
   }
 })
 </script>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <h1>Products</h1>
+  <ProductCards :products="products" />
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import ProductsModel from '~/api/models/products/productsModel'
+import { allProducts } from '~/api/controllers/products/products.controller'
+import { productsStore } from '~/store/products'
+import ProductCards from '~/components/ProductCards/ProductCards.vue'
+
+const products = ref<ProductsModel[]>([])
+onMounted(async () => {
+  try {
+    console.log('productsStore().generalProductsModel: ', productsStore().generalProductsModel)
+    products.value = productsStore().generalProductsModel
+    if (products.value.length == 0) {
+      products.value = await allProducts()
+      productsStore().setGeneralUserModel(products.value)
+    }
+  } catch (error) {
+    console.error('Error fetching products:', error)
+  }
+})
 </script>
