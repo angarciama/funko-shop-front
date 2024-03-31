1161cb788dc0220ff81f8ed2e8a1810c5e0c4c00
angarciama <angarciama@unal.edu.co>
2024-03-30 22:04:46 -0500

    [Admin] -products.controller: config fetch

--- api/controllers/products/products.controller.ts
+++ api/controllers/products/products.controller.ts
@@ -3,7 +3,16 @@ export const allProducts = async (): Promise<ProductsModel[]> => {
     try {
         const config = useRuntimeConfig()
 
-        const response = await fetch(config.public.apiBackUrl + '/api/products/all')
+        const response = await fetch(
+            config.public.apiBackUrl + '/api/products/all',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+        
         if (!response.ok) {
             throw new Error('Error fetching products')
         }
